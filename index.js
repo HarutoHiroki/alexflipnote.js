a0cee389be5f18546894a6a755912e05398edf02
HarutoHiroki <47408169+HarutoHiroki@users.noreply.github.com>
2020-06-17 11:09:53 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,72 @@
+const {get} = require('https');
+const {URL, URLSearchParams} = require('url');
+const endpoints = require('./endpoints.json');
+const { isObject } = require('util');
+//const fsp = require('fs/promises')
+
+function getContent(url) {
+  return new Promise((resolve, reject) => {
+    get(url, (res) => {
+      const {statusCode} = res;
+      if(statusCode !== 200) {
+        res.resume();
+        reject(`Request failed. Status code: ${statusCode}`);
+      }
+      res.setEncoding('utf8');
+      let rawData = '';
+      res.on('data', (chunk) => {rawData += chunk});
+      res.on('end', () => {
+        try {
+          if (/^[\],:{}\s]*$/.test(rawData.replace(/\\["\\\/bfnrtu]/g, '@').
+          replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').
+          replace(/(?:^|:|,)(?:\s*\[)+/g, ''))) {
+            const parsedData = JSON.parse(rawData);
+            resolve(parsedData);
+          }
+          else {
+            resolve(url)
+          }
+        } catch(e) {
+          reject(`Error: ${e.message}`);
+        }
+      });
+    }).on('error', (err) => {
+      reject(`Error: ${err.message}`);
+    })
+  });
+}
+
+class AlexClient {
+  constructor() {
+    let self = this;
+    self.image = {};
+    self.others = {};
+    let baseURL = 'https://api.alexflipnote.dev';
+    Object.keys(endpoints.image).forEach(async (endpoint) => {
+      self.image[endpoint] = async function (queryParams = '') {
+        let url = new URL(`${baseURL}${endpoints.image[endpoint]}`);
+        queryParams !== '' ? url.search = new URLSearchParams(queryParams) : '';
+        return await getContent(url.toString());
+        };
+    });
+    Object.keys(endpoints.others).forEach( async (endpoint) => {
+      self.others[endpoint] = async function (params = '') {
+        let url = new URL(`${baseURL}${endpoints.others[endpoint]}`);
+        if(endpoints.others[endpoint].includes("color")){
+          if(/^[0-9A-F]{6}$/i.test(params.toUpperCase())){
+            url = url.toString()+params
+            return await getContent(url);
+          }else{
+            return console.error("Not a valid hex value")
+          }
+        }
+        if(endpoints.others[endpoint].includes("steam")){
+          url = url.toString()+params
+          return await getContent(url);
+        }
+      };
+    });
+  }
+}
+
+module.exports = AlexClient;
---
+++ typings/index.d.ts
@@ -0,0 +1,91 @@
+//List all the functions
+declare class AlexClient {
+  image: {
+    birb():Promise<AlexClient.AlexRequestResults>;
+    cats():Promise<AlexClient.AlexRequestResults>;
+    dogs():Promise<AlexClient.AlexRequestResults>;
+    sadcat():Promise<AlexClient.AlexRequestResults>;
+    amiajoke(opts: AlexClient.AlexImgQueryParams):Promise<AlexClient.AlexResult>;
+    bad(opts: AlexClient.AlexImgQueryParams):Promise<AlexClient.AlexResult>;
+    joke(opts: AlexClient.AlexImgQueryParams):Promise<AlexClient.AlexResult>;
+    salty(opts: AlexClient.AlexImgQueryParams):Promise<AlexClient.AlexResult>;
+    blur(opts: AlexClient.AlexImgQueryParams):Promise<AlexClient.AlexResult>;
+    invert(opts: AlexClient.AlexImgQueryParams):Promise<AlexClient.AlexResult>;
+    bnW(opts: AlexClient.AlexImgQueryParams):Promise<AlexClient.AlexResult>;
+    deepfry(opts: AlexClient.AlexImgQueryParams):Promise<AlexClient.AlexResult>;
+    pixelate(opts: AlexClient.AlexImgQueryParams):Promise<AlexClient.AlexResult>;
+    magik(opts: AlexClient.AlexImgQueryParams):Promise<AlexClient.AlexResult>;
+    jpegify(opts: AlexClient.AlexImgQueryParams):Promise<AlexClient.AlexResult>;
+    snow(opts: AlexClient.AlexImgQueryParams):Promise<AlexClient.AlexResult>;
+    gay(opts: AlexClient.AlexImgQueryParams):Promise<AlexClient.AlexResult>;
+    communist(opts: AlexClient.AlexImgQueryParams):Promise<AlexClient.AlexResult>;
+    colorify(opts: AlexClient.AlexImgQueryParams):Promise<AlexClient.AlexResult>;
+    floor(opts: AlexClient.AlexImgQueryParams):Promise<AlexClient.AlexResult>;
+    scroll(opts: AlexClient.AlexFmlResults):Promise<AlexClient.AlexResult>;
+    facts(opts: AlexClient.AlexFmlResults):Promise<AlexClient.AlexResult>;
+    calling(opts: AlexClient.AlexFmlResults):Promise<AlexClient.AlexResult>;
+    captcha(opts: AlexClient.AlexFmlResults):Promise<AlexClient.AlexResult>;
+    achievement(opts: AlexClient.AlexTxtQueryParams):Promise<AlexClient.AlexResult>;
+    challenge(opts: AlexClient.AlexTxtQueryParams):Promise<AlexClient.AlexResult>;
+    supreme(opts: AlexClient.AlexTxtQueryParams):Promise<AlexClient.AlexResult>;
+    didyoumean(opts: AlexClient.AlexTopBottomText):Promise<AlexClient.AlexResult>;
+    drake(opts: AlexClient.AlexTopBottomText):Promise<AlexClient.AlexResult>;
+    pornhub(opts: AlexClient.AlexPH):Promise<AlexClient.AlexResult>;
+    ship(opts: AlexClient.AlexS):Promise<AlexClient.AlexResult>;
+    trash(opts: AlexClient.AlexT):Promise<AlexClient.AlexResult>;
+    fml():Promise<AlexClient.AlexFmlResults>;
+  }
+  others: {
+    color(opts: AlexClient.AlexColor):Promise<AlexClient.AlexBody>;
+    steam(opts: AlexClient.AlexColor):Promise<AlexClient.AlexBody>;
+  }
+}
+
+export = AlexClient;
+
+
+declare namespace AlexClient {
+  //Help create options interface for the few functions that need it
+  export interface AlexImgQueryParams {
+    image: string;
+    text?: string;
+    c?: string;
+    b?: string;
+  }
+  export interface AlexTxtQueryParams {
+    text: string;
+    icon?: string;
+    dark?: string;
+    light?: string;
+  }
+  export interface AlexRequestResults {
+    file: string;
+  }
+  export interface AlexFmlResults {
+    text: string;
+  }
+  export interface AlexBody {
+    body: string;
+  }
+  export interface AlexPH {
+    text1: string;
+    text2: string;
+  }
+  export interface AlexS {
+    user1: string;
+    user2: string;
+  }
+  export interface AlexT {
+    face: string;
+    trash: string;
+  }
+  export interface AlexColor {
+    text: string;
+  }
+  export interface AlexTopBottomText {
+    top: string;
+    bottom: string;
+    ayano?: string;
+  }
+  export type AlexResult = Buffer 
+}
